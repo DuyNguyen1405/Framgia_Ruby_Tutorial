a4f778dcd57fc48f3380c3ef7fd84aa2578e7fa9
Duy <Duyso1000@gmail.com>
2017-06-21 11:31:13 +0700

    Login + Code fix 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,20 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  class << User
+
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+  end
+
   private
 
   def downcase
