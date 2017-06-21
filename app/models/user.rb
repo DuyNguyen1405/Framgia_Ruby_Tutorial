63c684255550aa959d5df081424e232feb31a2c3
Duy <Duyso1000@gmail.com>
2017-06-21 16:56:26 +0700

    Advanced login + Code fix 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
   before_save :downcase
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,7 +8,8 @@ class User < ApplicationRecord
   validates :name, presence: true,
     length: {maximum: Settings.name_max_length}
   validates :password, presence: true,
-    length: {minimum: Settings.password_max_length}
+    length: {minimum: Settings.password_max_length},
+    allow_nil: true
   validates :email, presence: true,
     length: {maximum: Settings.email_max_length},
     format: {with: VALID_EMAIL_REGEX},
@@ -27,6 +29,24 @@ class User < ApplicationRecord
       BCrypt::Password.create string, cost: cost
     end
 
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest.present?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 
   private
