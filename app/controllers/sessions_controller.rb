9b5d659671c50dd1d98549983465fd9c930f5f8c
Duy <Duyso1000@gmail.com>
2017-06-22 16:25:55 +0700

    Activate account + Code fix 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
     session = params[:session]
     user = User.find_by email: session[:email].downcase
     if user && user.authenticate(session[:password])
-      log_in user
-      session[:remember_me] == "1" ? remember(user) : forget(user)
-      redirect_to user
+      if user.activated?
+        log_in user
+        session[:remember_me] == "1" ? remember(user) : forget(user)
+        redirect_to user
+      else
+        flash[:warning] = t "account_not_activated"
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "invalid_email_password"
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622082938) do
+ActiveRecord::Schema.define(version: 20170623032016) do
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,11 +20,14 @@ ActiveRecord::Schema.define(version: 20170622082938) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
